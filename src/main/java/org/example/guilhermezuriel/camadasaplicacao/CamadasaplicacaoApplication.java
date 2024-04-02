51fe07ea4bfc18bb46d000d00ec4ceaccba3e1f7
Guilherme Zuriel <guizuriel.dev@gmail.com>
2024-04-02 14:53:40 -0300

    First commit

---
+++ src/main/java/org/example/guilhermezuriel/camadasaplicacao/CamadasaplicacaoApplication.java
@@ -0,0 +1,13 @@
+package org.example.guilhermezuriel.camadasaplicacao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CamadasaplicacaoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CamadasaplicacaoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/guilhermezuriel/camadasaplicacao/CamadasaplicacaoApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.guilhermezuriel.camadasaplicacao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CamadasaplicacaoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
