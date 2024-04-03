22d1f7275f4e459bf15370e5fe35dc5b1ab387c4
Guilherme Zuriel <guizuriel.dev@gmail.com>
2024-04-03 09:01:54 -0300

    Not Working branch

--- src/main/java/org/example/guilhermezuriel/camadasaplicacao/controller/UserController.java
+++ src/main/java/org/example/guilhermezuriel/camadasaplicacao/controller/UserController.java
@@ -1,11 +1,8 @@
 package org.example.guilhermezuriel.camadasaplicacao.controller;
 
 import org.example.guilhermezuriel.camadasaplicacao.dtos.UserDTO;
-import org.example.guilhermezuriel.camadasaplicacao.entities.User;
 import org.example.guilhermezuriel.camadasaplicacao.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,9 +17,8 @@ public class UserController {
 
 
     @GetMapping("/{id}")
-    public ResponseEntity<Object> findById(@PathVariable Long id){
-        User user = service.findById(id);
-        return ResponseEntity.status(HttpStatus.ACCEPTED).body(user);
+    public UserDTO findById(@PathVariable Long id){
+        return service.findById(id);
     }
 
 }
